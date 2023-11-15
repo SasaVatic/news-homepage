0ae29c0f7bc0a2eee137062c8664bdfeda24a863
Sasa Vatic <zeklbike@gmail.com>
2023-11-15 22:20:15 +0100

    fix url for images

--- src/server.js
+++ src/server.js
@@ -38,17 +38,17 @@ createServer({
                     },
                     cards: [
                         {
-                            imgURL: '/images/cards/image-retro-pcs.jpg',
+                            imgURL: './images/cards/image-retro-pcs.jpg',
                             title: 'Reviving Retro PCs',
                             text: 'What happens when old PCs are given modern upgrades?'
                         },
                         {
-                            imgURL: '/images/cards/image-top-laptops.jpg',
+                            imgURL: './images/cards/image-top-laptops.jpg',
                             title: 'Top 10 Laptops of 2022',
                             text: 'Our best picks for various needs and budgets.'
                         },
                         {
-                            imgURL: '/images/cards/image-gaming-growth.jpg',
+                            imgURL: './images/cards/image-gaming-growth.jpg',
                             title: 'The Growth of Gaming',
                             text: 'How the pandemic has sparked fresh opportunities.'
                         }
