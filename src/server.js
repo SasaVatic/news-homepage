521be5bca2ce506591cc02909e17dcc6219d998f
Sasa Vatic <zeklbike@gmail.com>
2023-11-15 14:59:54 +0100

    hero created

--- src/server.js
+++ src/server.js
@@ -14,6 +14,11 @@ createServer({
                         { label: 'Trending', href: '#' },
                         { label: 'Categories', href: '#' }
                     ]
+                },
+                hero: {
+                    title: 'The Bright Future of Web 3.0?',
+                    text: 'We dive into the next evolution of the web that claims to put the power of the platforms back into the hands of the people. But is it really fulfilling its promise?',
+                    btnText: 'Read more'
                 }
             }
         })
